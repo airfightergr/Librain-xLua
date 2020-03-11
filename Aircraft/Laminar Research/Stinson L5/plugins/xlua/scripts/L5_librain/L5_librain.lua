57455daa7d271563f597f5ce860e446e57f9479e
todirbg <todirbg@yahoo.com>
2020-03-12 01:53:48 +0200

    Edited c172, added L5

--- Aircraft/Laminar Research/Stinson L5/plugins/xlua/scripts/L5_librain/L5_librain.lua
+++ Aircraft/Laminar Research/Stinson L5/plugins/xlua/scripts/L5_librain/L5_librain.lua
@@ -69,6 +69,26 @@ backGlass_load             = find_dataref("librain/glass_2/obj/load")
 backGlass_loaded           = find_dataref("librain/glass_2/obj/loaded")
 backGlass_wind_normal      = find_dataref("librain/glass_2/wind_normal")
 
+topGlass_slant            = find_dataref("librain/glass_3/slant_factor")
+topGlass_tp_x             = find_dataref("librain/glass_3/thrust_point/x")
+topGlass_tp_y             = find_dataref("librain/glass_3/thrust_point/y")
+topGlass_thrust_factor    = find_dataref("librain/glass_3/thrust_factor")
+topGlass_max_thrust       = find_dataref("librain/glass_3/max_thrust")
+topGlass_gp_x             = find_dataref("librain/glass_3/gravity_point/x")
+topGlass_gp_y             = find_dataref("librain/glass_3/gravity_point/y")
+topGlass_gravity_factor   = find_dataref("librain/glass_3/gravity_factor")
+topGlass_wp_x             = find_dataref("librain/glass_3/wind_point/x")
+topGlass_wp_y             = find_dataref("librain/glass_3/wind_point/y")
+topGlass_wind_factor      = find_dataref("librain/glass_3/wind_factor")
+topGlass_max_tas          = find_dataref("librain/glass_3/max_tas")
+topGlass_obj              = find_dataref("librain/glass_3/obj/filename")
+topGlass_obj_offX         = find_dataref("librain/glass_3/obj/pos_offset/x")
+topGlass_obj_offY         = find_dataref("librain/glass_3/obj/pos_offset/y")
+topGlass_obj_offZ         = find_dataref("librain/glass_3/obj/pos_offset/z")
+topGlass_load             = find_dataref("librain/glass_3/obj/load")
+topGlass_loaded           = find_dataref("librain/glass_3/obj/loaded")
+topGlass_wind_normal      = find_dataref("librain/glass_3/wind_normal")
+
 z_obj_obj0_name             = find_dataref("librain/z_depth_obj_0/filename")
 z_obj_obj0_load             = find_dataref("librain/z_depth_obj_0/load")
 z_obj_obj0_loaded           = find_dataref("librain/z_depth_obj_0/loaded")
@@ -132,40 +152,54 @@ z_obj_obj8_offX             = find_dataref("librain/z_depth_obj_8/pos_offset/x")
 z_obj_obj8_offY             = find_dataref("librain/z_depth_obj_8/pos_offset/y")
 z_obj_obj8_offZ             = find_dataref("librain/z_depth_obj_8/pos_offset/z")
 
--- z_obj_obj9_name             = find_dataref("librain/z_depth_obj_9/filename")
--- z_obj_obj9_load             = find_dataref("librain/z_depth_obj_9/load")
--- z_obj_obj9_loaded           = find_dataref("librain/z_depth_obj_9/loaded")
--- z_obj_obj9_offX             = find_dataref("librain/z_depth_obj_9/pos_offset/x")
--- z_obj_obj9_offY             = find_dataref("librain/z_depth_obj_9/pos_offset/y")
--- z_obj_obj9_offZ             = find_dataref("librain/z_depth_obj_9/pos_offset/z")
+z_obj_obj9_name             = find_dataref("librain/z_depth_obj_9/filename")
+z_obj_obj9_load             = find_dataref("librain/z_depth_obj_9/load")
+z_obj_obj9_loaded           = find_dataref("librain/z_depth_obj_9/loaded")
+z_obj_obj9_offX             = find_dataref("librain/z_depth_obj_9/pos_offset/x")
+z_obj_obj9_offY             = find_dataref("librain/z_depth_obj_9/pos_offset/y")
+z_obj_obj9_offZ             = find_dataref("librain/z_depth_obj_9/pos_offset/z")
 
--- z_obj_obj10_name             = find_dataref("librain/z_depth_obj_10/filename")
--- z_obj_obj10_load             = find_dataref("librain/z_depth_obj_10/load")
--- z_obj_obj10_loaded           = find_dataref("librain/z_depth_obj_10/loaded")
--- z_obj_obj10_offX             = find_dataref("librain/z_depth_obj_10/pos_offset/x")
--- z_obj_obj10_offY             = find_dataref("librain/z_depth_obj_10/pos_offset/y")
--- z_obj_obj10_offZ             = find_dataref("librain/z_depth_obj_10/pos_offset/z")
+z_obj_obj10_name             = find_dataref("librain/z_depth_obj_10/filename")
+z_obj_obj10_load             = find_dataref("librain/z_depth_obj_10/load")
+z_obj_obj10_loaded           = find_dataref("librain/z_depth_obj_10/loaded")
+z_obj_obj10_offX             = find_dataref("librain/z_depth_obj_10/pos_offset/x")
+z_obj_obj10_offY             = find_dataref("librain/z_depth_obj_10/pos_offset/y")
+z_obj_obj10_offZ             = find_dataref("librain/z_depth_obj_10/pos_offset/z")
 
--- z_obj_obj11_name             = find_dataref("librain/z_depth_obj_11/filename")
--- z_obj_obj11_load             = find_dataref("librain/z_depth_obj_11/load")
--- z_obj_obj11_loaded           = find_dataref("librain/z_depth_obj_11/loaded")
--- z_obj_obj11_offX             = find_dataref("librain/z_depth_obj_11/pos_offset/x")
--- z_obj_obj11_offY             = find_dataref("librain/z_depth_obj_11/pos_offset/y")
--- z_obj_obj11_offZ             = find_dataref("librain/z_depth_obj_11/pos_offset/z")
+z_obj_obj11_name             = find_dataref("librain/z_depth_obj_11/filename")
+z_obj_obj11_load             = find_dataref("librain/z_depth_obj_11/load")
+z_obj_obj11_loaded           = find_dataref("librain/z_depth_obj_11/loaded")
+z_obj_obj11_offX             = find_dataref("librain/z_depth_obj_11/pos_offset/x")
+z_obj_obj11_offY             = find_dataref("librain/z_depth_obj_11/pos_offset/y")
+z_obj_obj11_offZ             = find_dataref("librain/z_depth_obj_11/pos_offset/z")
+
+z_obj_obj12_name             = find_dataref("librain/z_depth_obj_12/filename")
+z_obj_obj12_load             = find_dataref("librain/z_depth_obj_12/load")
+z_obj_obj12_loaded           = find_dataref("librain/z_depth_obj_12/loaded")
+z_obj_obj12_offX             = find_dataref("librain/z_depth_obj_12/pos_offset/x")
+z_obj_obj12_offY             = find_dataref("librain/z_depth_obj_12/pos_offset/y")
+z_obj_obj12_offZ             = find_dataref("librain/z_depth_obj_12/pos_offset/z")
+
+z_obj_obj13_name             = find_dataref("librain/z_depth_obj_13/filename")
+z_obj_obj13_load             = find_dataref("librain/z_depth_obj_13/load")
+z_obj_obj13_loaded           = find_dataref("librain/z_depth_obj_13/loaded")
+z_obj_obj13_offX             = find_dataref("librain/z_depth_obj_13/pos_offset/x")
+z_obj_obj13_offY             = find_dataref("librain/z_depth_obj_13/pos_offset/y")
+z_obj_obj13_offZ             = find_dataref("librain/z_depth_obj_13/pos_offset/z")
 
 rain_numglass               = find_dataref("librain/num_glass_use")
 rain_init                   = find_dataref("librain/initialize")
 
   external_view_dr 		  = find_dataref("sim/graphics/view/view_is_external")
   rain_dr 				  = find_dataref("sim/weather/rain_percent")
-  VR_dr 				  = find_dataref("sim/graphics/VR/enabled")
+--  VR_dr 				  = find_dataref("sim/graphics/VR/enabled")
 
 local rain_inited = 0
 
 function init()
 rain_init = 0
 
-	log("[C172_DEBUG]: " .. path())   -- write in the log.txt the path for debugging
+	log("[L5_DEBUG]: " .. path())   -- write in the log.txt the path for debugging
 	
 	--obj_path						= "../../../objects/librain/"
 	librain_path    				= acfFolder .. "/objects/librain/"	--"Aircraft/Laminar Research/Cessna 172SP/objects/librain/"
@@ -174,32 +208,38 @@ rain_init = 0
 	local frontGlass	             = librain_path .. "frontGlass.obj"
 	local sideGlass	            	 = librain_path .. "sideGlass.obj"
 	local backGlass		             = librain_path .. "backGlass.obj"
-	local mask0_obj                  = mask_path .. "cockpit_panel.obj"
-	local mask1_obj                  = mask_path .. "instruments/compass/compass_mag_glrshld.obj"
-	local mask2_obj                  = mask_path .. "cockpit_1.obj"
-	local mask3_obj                  = mask_path .. "cockpit_2.obj"
-	local mask4_obj                  = mask_path .. "cockpit_seats_front.obj"
-	local mask5_obj                  = mask_path .. "cockpit_seats_rear.obj"
-	local mask6_obj                  = mask_path .. "fuselage.obj"
-	local mask7_obj                  = mask_path .. "wings.obj"
-	local mask8_obj                  = mask_path .. "cockpit_panel_G1000.obj"
-	
-	rain_numglass = 3
+	local topGlass		             = librain_path .. "topGlass.obj"
+	local mask0_obj                  = mask_path .. "panel.obj"
+	local mask1_obj                  = mask_path .. "inn_1.obj"
+	local mask2_obj                  = mask_path .. "inn_2.obj"
+	local mask3_obj                  = mask_path .. "seat.obj"
+	local mask4_obj                  = mask_path .. "instruments/xPad/xPad_arm.obj"
+	local mask5_obj                  = mask_path .. "fuse.obj"
+	local mask6_obj                  = mask_path .. "wings.obj"
+	local mask7_obj                  = mask_path .. "instruments/compass/compass_mag.obj"
+	local mask8_obj                  = mask_path .. "instruments/airspeed/airspeed.obj"
+	local mask9_obj                  = mask_path .. "instruments/altimeter/altimeter_ag.obj"	
+	local mask10_obj                  = mask_path .. "instruments/engine_RPM/engine_RPM_ag.obj"	
+	local mask11_obj                  = mask_path .. "instruments/turn_slip/turn_slip_ga.obj"	
+	local mask12_obj                  = mask_path .. "instruments/variometer/variometer_ag.obj"	
+	local mask13_obj                  = mask_path .. "instruments/cht_oilt_oilp_amps/cht_oilt_oilp_amps.obj"	
+
+	rain_numglass = 4
 	
 	rain_verbose = 0
 	rain_debug_draw = 0
 	rain_wipers = 0
 	
-	frontGlass_slant = 0.9
+	frontGlass_slant = 0.8
 	frontGlass_tp_x = 0.5
-	frontGlass_tp_y = 0.0
+	frontGlass_tp_y = 0.15
 	frontGlass_thrust_factor = 1
 	frontGlass_max_thrust = 2500
 	frontGlass_gp_x = 0.5
-	frontGlass_gp_y = 1.2
+	frontGlass_gp_y = 0.8
 	frontGlass_gravity_factor = 0.7
 	frontGlass_wp_x = 0.5
-	frontGlass_wp_y = 0.25
+	frontGlass_wp_y = 0.32
 	frontGlass_wind_factor = 1.0
 	frontGlass_wind_normal = 1.0
 	frontGlass_max_tas = 60
@@ -210,15 +250,15 @@ rain_init = 0
 	frontGlass_load = 1
 	
 	sideGlass_slant = 0.7
-	sideGlass_tp_x = -3
-	sideGlass_tp_y = 0.4
-	sideGlass_thrust_factor = 0.5
+	sideGlass_tp_x = -5
+	sideGlass_tp_y = 0.5
+	sideGlass_thrust_factor = 0.3
 	sideGlass_max_thrust = 1800
 	sideGlass_gp_x = 0.5
-	sideGlass_gp_y = 5.0
-	sideGlass_gravity_factor = 0.5
+	sideGlass_gp_y = 3
+	sideGlass_gravity_factor = 0.8
 	sideGlass_wp_x = -5
-	sideGlass_wp_y = 0.4
+	sideGlass_wp_y = 0.5
 	sideGlass_wind_factor = 1.0
 	sideGlass_wind_normal = 0.0
 	sideGlass_max_tas = 60
@@ -230,11 +270,11 @@ rain_init = 0
 	
 	backGlass_slant = 0.8
 	backGlass_tp_x = 0.5
-	backGlass_tp_y = 0.8
+	backGlass_tp_y = 1.0
 	backGlass_thrust_factor = 0.2
 	backGlass_max_thrust = 1300
 	backGlass_gp_x = 0.5
-	backGlass_gp_y = 0.8
+	backGlass_gp_y = 1
 	backGlass_gravity_factor = 0.5
 	backGlass_wp_x = 0.5
 	backGlass_wp_y = 0.8
@@ -246,6 +286,25 @@ rain_init = 0
 	backGlass_obj_offY = 0.0
 	backGlass_obj_offZ = 0.0
 	backGlass_load = 1
+	
+	topGlass_slant = 1
+	topGlass_tp_x = 0.5
+	topGlass_tp_y = 2
+	topGlass_thrust_factor = 0.4
+	topGlass_max_thrust = 1600
+	topGlass_gp_x = 0.5
+	topGlass_gp_y = 0.95
+	topGlass_gravity_factor = 0.2
+	topGlass_wp_x = 0.5
+	topGlass_wp_y = 3
+	topGlass_wind_factor = 1.0
+	topGlass_wind_normal = 0.0
+	topGlass_max_tas = 60
+	topGlass_obj = topGlass
+	topGlass_obj_offX = 0.0
+	topGlass_obj_offY = 0.0
+	topGlass_obj_offZ = 0.0
+	topGlass_load = 1
 
 
 	z_obj_obj0_name = mask0_obj
@@ -303,23 +362,36 @@ rain_init = 0
 	z_obj_obj8_offZ = 0
 	z_obj_obj8_load = 1
 
-	-- z_obj_obj9_name = mask9_obj
-	-- z_obj_obj9_offX = 0
-	-- z_obj_obj9_offY = 0
-	-- z_obj_obj9_offZ = 0
-	-- z_obj_obj9_load = 1
+	z_obj_obj9_name = mask9_obj
+	z_obj_obj9_offX = 0
+	z_obj_obj9_offY = 0
+	z_obj_obj9_offZ = 0
+	z_obj_obj9_load = 1
 	
-	-- z_obj_obj10_name = mask10_obj
-	-- z_obj_obj10_offX = 0
-	-- z_obj_obj10_offY = 0
-	-- z_obj_obj10_offZ = 0
-	-- z_obj_obj10_load = 1
+	z_obj_obj10_name = mask10_obj
+	z_obj_obj10_offX = 0
+	z_obj_obj10_offY = 0
+	z_obj_obj10_offZ = 0
+	z_obj_obj10_load = 1
 	
-	-- z_obj_obj11_name = mask11_obj
-	-- z_obj_obj11_offX = 0
-	-- z_obj_obj11_offY = 0
-	-- z_obj_obj11_offZ = 0
-	-- z_obj_obj11_load = 1	
+	z_obj_obj11_name = mask11_obj
+	z_obj_obj11_offX = 0
+	z_obj_obj11_offY = 0
+	z_obj_obj11_offZ = 0
+	z_obj_obj11_load = 1	
+	
+	z_obj_obj12_name = mask12_obj
+	z_obj_obj12_offX = 0
+	z_obj_obj12_offY = 0
+	z_obj_obj12_offZ = 0
+	z_obj_obj12_load = 1
+	
+	z_obj_obj13_name = mask13_obj
+	z_obj_obj13_offX = 0
+	z_obj_obj13_offY = 0
+	z_obj_obj13_offZ = 0
+	z_obj_obj13_load = 1	
+
 
 
 	--rain_init = 1
@@ -336,9 +408,9 @@ end
 
 --------------------------------- REGULAR RUNTIME ---------------------------------
 function after_physics()
-	if rain_inited == 1 and rain_init == 0 and rain_dr > 0  and VR_dr == 0 and external_view_dr == 0 then
+	if rain_inited == 1 and rain_init == 0 and rain_dr > 0  and external_view_dr == 0 then
 		rain_init = 1
-	elseif rain_init == 1 and (rain_dr == 0 or external_view_dr == 1 or VR_dr == 1) then
+	elseif rain_init == 1 and (rain_dr == 0 or external_view_dr == 1) then
 		rain_init = 0
 	end
 
