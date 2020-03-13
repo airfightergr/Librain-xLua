d6e59627c80456acf0d9a1d3ac1f2e8156bd1198
todirbg <todirbg@yahoo.com>
2020-03-13 14:29:14 +0200

    Streched UV maps, added Propstrike Bush detection
    Streched UV maps to maximize used area
    Added Propstrike Bush variant detection and fixed cockpit.obj
    Removed VR detection

--- Aircraft/Laminar Research/Cessna 172SP/plugins/xlua/scripts/c172_librain/c172_librain.lua
+++ Aircraft/Laminar Research/Cessna 172SP/plugins/xlua/scripts/c172_librain/c172_librain.lua
@@ -132,33 +132,35 @@ z_obj_obj8_offX             = find_dataref("librain/z_depth_obj_8/pos_offset/x")
 z_obj_obj8_offY             = find_dataref("librain/z_depth_obj_8/pos_offset/y")
 z_obj_obj8_offZ             = find_dataref("librain/z_depth_obj_8/pos_offset/z")
 
--- z_obj_obj9_name             = find_dataref("librain/z_depth_obj_9/filename")
--- z_obj_obj9_load             = find_dataref("librain/z_depth_obj_9/load")
--- z_obj_obj9_loaded           = find_dataref("librain/z_depth_obj_9/loaded")
--- z_obj_obj9_offX             = find_dataref("librain/z_depth_obj_9/pos_offset/x")
--- z_obj_obj9_offY             = find_dataref("librain/z_depth_obj_9/pos_offset/y")
--- z_obj_obj9_offZ             = find_dataref("librain/z_depth_obj_9/pos_offset/z")
+z_obj_obj9_name             = find_dataref("librain/z_depth_obj_9/filename")
+z_obj_obj9_load             = find_dataref("librain/z_depth_obj_9/load")
+z_obj_obj9_loaded           = find_dataref("librain/z_depth_obj_9/loaded")
+z_obj_obj9_offX             = find_dataref("librain/z_depth_obj_9/pos_offset/x")
+z_obj_obj9_offY             = find_dataref("librain/z_depth_obj_9/pos_offset/y")
+z_obj_obj9_offZ             = find_dataref("librain/z_depth_obj_9/pos_offset/z")
 
--- z_obj_obj10_name             = find_dataref("librain/z_depth_obj_10/filename")
--- z_obj_obj10_load             = find_dataref("librain/z_depth_obj_10/load")
--- z_obj_obj10_loaded           = find_dataref("librain/z_depth_obj_10/loaded")
--- z_obj_obj10_offX             = find_dataref("librain/z_depth_obj_10/pos_offset/x")
--- z_obj_obj10_offY             = find_dataref("librain/z_depth_obj_10/pos_offset/y")
--- z_obj_obj10_offZ             = find_dataref("librain/z_depth_obj_10/pos_offset/z")
+z_obj_obj10_name             = find_dataref("librain/z_depth_obj_10/filename")
+z_obj_obj10_load             = find_dataref("librain/z_depth_obj_10/load")
+z_obj_obj10_loaded           = find_dataref("librain/z_depth_obj_10/loaded")
+z_obj_obj10_offX             = find_dataref("librain/z_depth_obj_10/pos_offset/x")
+z_obj_obj10_offY             = find_dataref("librain/z_depth_obj_10/pos_offset/y")
+z_obj_obj10_offZ             = find_dataref("librain/z_depth_obj_10/pos_offset/z")
 
--- z_obj_obj11_name             = find_dataref("librain/z_depth_obj_11/filename")
--- z_obj_obj11_load             = find_dataref("librain/z_depth_obj_11/load")
--- z_obj_obj11_loaded           = find_dataref("librain/z_depth_obj_11/loaded")
--- z_obj_obj11_offX             = find_dataref("librain/z_depth_obj_11/pos_offset/x")
--- z_obj_obj11_offY             = find_dataref("librain/z_depth_obj_11/pos_offset/y")
--- z_obj_obj11_offZ             = find_dataref("librain/z_depth_obj_11/pos_offset/z")
+z_obj_obj11_name             = find_dataref("librain/z_depth_obj_11/filename")
+z_obj_obj11_load             = find_dataref("librain/z_depth_obj_11/load")
+z_obj_obj11_loaded           = find_dataref("librain/z_depth_obj_11/loaded")
+z_obj_obj11_offX             = find_dataref("librain/z_depth_obj_11/pos_offset/x")
+z_obj_obj11_offY             = find_dataref("librain/z_depth_obj_11/pos_offset/y")
+z_obj_obj11_offZ             = find_dataref("librain/z_depth_obj_11/pos_offset/z")
 
 rain_numglass               = find_dataref("librain/num_glass_use")
 rain_init                   = find_dataref("librain/initialize")
 
   external_view_dr 		  = find_dataref("sim/graphics/view/view_is_external")
   rain_dr 				  = find_dataref("sim/weather/rain_percent")
-  VR_dr 				  = find_dataref("sim/graphics/VR/enabled")
+ -- VR_dr 				  = find_dataref("sim/graphics/VR/enabled")
+   acf_notes_dr 				  = find_dataref("sim/aircraft/view/acf_notes")
+ -- acf_descrip_dr 				  = find_dataref("sim/aircraft/view/acf_descrip")
 
 local rain_inited = 0
 
@@ -174,7 +176,8 @@ rain_init = 0
 	local frontGlass	             = librain_path .. "frontGlass.obj"
 	local sideGlass	            	 = librain_path .. "sideGlass.obj"
 	local backGlass		             = librain_path .. "backGlass.obj"
-	local mask0_obj                  = mask_path .. "cockpit_panel.obj"
+	
+	local mask0_obj                  = mask_path .. "cockpit_panel_G1000.obj"
 	local mask1_obj                  = mask_path .. "instruments/compass/compass_mag_glrshld.obj"
 	local mask2_obj                  = mask_path .. "cockpit_1.obj"
 	local mask3_obj                  = mask_path .. "cockpit_2.obj"
@@ -182,8 +185,10 @@ rain_init = 0
 	local mask5_obj                  = mask_path .. "cockpit_seats_rear.obj"
 	local mask6_obj                  = mask_path .. "fuselage.obj"
 	local mask7_obj                  = mask_path .. "wings.obj"
-	local mask8_obj                  = mask_path .. "cockpit_panel_G1000.obj"
-	
+	local mask8_obj                  = mask_path .. "GarminAOAIndicator.obj"
+	local mask9_obj                  = mask_path .. "GarminAOAIndicatorLights.obj"
+	local mask10_obj                  = mask_path .. "CabinCargo.obj"
+--	local mask11_obj                  = mask_path .. ""	
 	rain_numglass = 3
 	
 	rain_verbose = 0
@@ -192,36 +197,36 @@ rain_init = 0
 	
 	frontGlass_slant = 0.9
 	frontGlass_tp_x = 0.5
-	frontGlass_tp_y = 0.0
+	frontGlass_tp_y = -0.7
 	frontGlass_thrust_factor = 1
 	frontGlass_max_thrust = 2500
 	frontGlass_gp_x = 0.5
-	frontGlass_gp_y = 1.2
-	frontGlass_gravity_factor = 0.7
+	frontGlass_gp_y = 2
+	frontGlass_gravity_factor = 1
 	frontGlass_wp_x = 0.5
-	frontGlass_wp_y = 0.25
+	frontGlass_wp_y = 0.0
 	frontGlass_wind_factor = 1.0
 	frontGlass_wind_normal = 1.0
-	frontGlass_max_tas = 60
+	frontGlass_max_tas = 80
 	frontGlass_obj = frontGlass
 	frontGlass_obj_offX = 0.0
 	frontGlass_obj_offY = 0.0
 	frontGlass_obj_offZ = 0.0
 	frontGlass_load = 1
 	
-	sideGlass_slant = 0.7
-	sideGlass_tp_x = -3
+	sideGlass_slant = 0.5
+	sideGlass_tp_x = -5
 	sideGlass_tp_y = 0.4
 	sideGlass_thrust_factor = 0.5
 	sideGlass_max_thrust = 1800
 	sideGlass_gp_x = 0.5
 	sideGlass_gp_y = 5.0
-	sideGlass_gravity_factor = 0.5
+	sideGlass_gravity_factor = 1
 	sideGlass_wp_x = -5
 	sideGlass_wp_y = 0.4
 	sideGlass_wind_factor = 1.0
-	sideGlass_wind_normal = 0.0
-	sideGlass_max_tas = 60
+	sideGlass_wind_normal = 0.7
+	sideGlass_max_tas = 80
 	sideGlass_obj = sideGlass
 	sideGlass_obj_offX = 0.0
 	sideGlass_obj_offY = 0.0
@@ -230,16 +235,16 @@ rain_init = 0
 	
 	backGlass_slant = 0.8
 	backGlass_tp_x = 0.5
-	backGlass_tp_y = 0.8
+	backGlass_tp_y = 1.5
 	backGlass_thrust_factor = 0.2
 	backGlass_max_thrust = 1300
 	backGlass_gp_x = 0.5
-	backGlass_gp_y = 0.8
-	backGlass_gravity_factor = 0.5
+	backGlass_gp_y = 1.5
+	backGlass_gravity_factor = 0.7
 	backGlass_wp_x = 0.5
-	backGlass_wp_y = 0.8
+	backGlass_wp_y = 1.5
 	backGlass_wind_factor = 1.0
-	backGlass_wind_normal = 0.0
+	backGlass_wind_normal = 0.8
 	backGlass_max_tas = 60
 	backGlass_obj = backGlass
 	backGlass_obj_offX = 0.0
@@ -278,12 +283,15 @@ rain_init = 0
 	z_obj_obj4_offY = 0
 	z_obj_obj4_offZ = 0
 	z_obj_obj4_load = 1
-	
-	z_obj_obj5_name = mask5_obj
-	z_obj_obj5_offX = 0
-	z_obj_obj5_offY = 0
-	z_obj_obj5_offZ = 0
-	z_obj_obj5_load = 1
+
+	if 	acf_notes_dr ~= "STOL" then --detect Propstrike Bush variant
+		z_obj_obj5_name = mask5_obj
+		z_obj_obj5_offX = 0
+		z_obj_obj5_offY = 0
+		z_obj_obj5_offZ = 0
+		z_obj_obj5_load = 1
+	end
+
 
 	z_obj_obj6_name = mask6_obj
 	z_obj_obj6_offX = 0
@@ -297,30 +305,32 @@ rain_init = 0
 	z_obj_obj7_offZ = 0
 	z_obj_obj7_load = 1
 
-	z_obj_obj8_name = mask8_obj
-	z_obj_obj8_offX = 0
-	z_obj_obj8_offY = 0
-	z_obj_obj8_offZ = 0
-	z_obj_obj8_load = 1
-
-	-- z_obj_obj9_name = mask9_obj
-	-- z_obj_obj9_offX = 0
-	-- z_obj_obj9_offY = 0
-	-- z_obj_obj9_offZ = 0
-	-- z_obj_obj9_load = 1
 	
-	-- z_obj_obj10_name = mask10_obj
-	-- z_obj_obj10_offX = 0
-	-- z_obj_obj10_offY = 0
-	-- z_obj_obj10_offZ = 0
-	-- z_obj_obj10_load = 1
+	if 	acf_notes_dr == "STOL" then --detect Propstrike Bush variant
+		z_obj_obj8_name = mask8_obj
+		z_obj_obj8_offX = 0
+		z_obj_obj8_offY = 0
+		z_obj_obj8_offZ = 0
+		z_obj_obj8_load = 1
+		
+		z_obj_obj9_name = mask9_obj
+		z_obj_obj9_offX = 0
+		z_obj_obj9_offY = 0
+		z_obj_obj9_offZ = 0
+		z_obj_obj9_load = 1
+		
+		z_obj_obj10_name = mask10_obj
+		z_obj_obj10_offX = 0
+		z_obj_obj10_offY = 0
+		z_obj_obj10_offZ = 0
+		z_obj_obj10_load = 1	
+	end
 	
 	-- z_obj_obj11_name = mask11_obj
 	-- z_obj_obj11_offX = 0
 	-- z_obj_obj11_offY = 0
 	-- z_obj_obj11_offZ = 0
 	-- z_obj_obj11_load = 1	
-
 
 	--rain_init = 1
 	rain_inited = 1
@@ -336,9 +346,9 @@ end
 
 --------------------------------- REGULAR RUNTIME ---------------------------------
 function after_physics()
-	if rain_inited == 1 and rain_init == 0 and rain_dr > 0  and VR_dr == 0 and external_view_dr == 0 then
+	if rain_inited == 1 and rain_init == 0 and rain_dr > 0 and external_view_dr == 0 then
 		rain_init = 1
-	elseif rain_init == 1 and (rain_dr == 0 or external_view_dr == 1 or VR_dr == 1) then
+	elseif rain_init == 1 and (rain_dr == 0 or external_view_dr == 1) then
 		rain_init = 0
 	end
 
